6aeb30e2e7e9e4d14081796780dba6092e212a43
Charles Olupot <olupotcharles@gmail.com>
2022-04-26 06:50:33 +0300

    Why

--- query.js
+++ query.js
@@ -68,7 +68,7 @@ from datavalue dv
   inner join categorycombo cc using(categorycomboid)
   inner join period p using(periodid)
   inner join periodtype pt using(periodtypeid)
-where de.uid = '$1'
+where de.uid = $1
   and p.startdate >= $2
   and p.enddate <= $3;`,
 			[args[0], args[1], args[2]]
